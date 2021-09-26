a9ae44fc580acb5d59b36276033ca43cca1c1f17
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2021-09-26 08:26:41 -0400

    Init stater app

--- api/src/main/java/com/harrydulaney/firebaseauthstarter/configuration/AppConfig.java
+++ api/src/main/java/com/harrydulaney/firebaseauthstarter/configuration/AppConfig.java
@@ -1,6 +1,5 @@
 package com.harrydulaney.firebaseauthstarter.configuration;
 
-import com.harrydulaney.firebaseauthstarter.model.UserPreferences;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -11,21 +10,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
-  @Bean
-  RestTemplate restTemplate() {
-    RestTemplate restTemplate = new RestTemplate();
-    restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-    restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-    return restTemplate;
-  }
-
-
-  @Bean
-  @Scope("singleton")
-  public UserPreferences userPreferences() {
-    UserPreferences preferences = new UserPreferences();
-    preferences.loadPreferences();
-    return preferences;
-  }
+    @Bean
+    RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+        return restTemplate;
+    }
 
 }
