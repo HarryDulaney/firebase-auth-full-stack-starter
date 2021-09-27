4dbf31472a91996dadf4634f8673a25c1fb67239
Harry G. Dulaney  IV <harrydulaney@gmail.com>
2021-09-27 18:07:53 -0400

    Init stater app

--- api/src/main/java/com/harrydulaney/firebaseauthstarter/utils/Constants.java
+++ api/src/main/java/com/harrydulaney/firebaseauthstarter/utils/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 
     /* ----------------------------------------- Cookies  ---------------------------------------------------- */
     public static final boolean CHECK_REVOKED = true;
-    public static final String SESSION_COOKIE = "session-file-com";
+    public static final String SESSION_COOKIE = "session-cookie-auth";
     public static final boolean STRICT_SERVER_SESSION_ENABLED = true;
     public static final String PARENT_COOKIE_PATH = "/";
     public static final boolean COOKIE_HTTP_ONLY_LOCAL = false;
